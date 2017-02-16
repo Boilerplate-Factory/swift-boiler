d88bca7903cddc06f5daf59708f2118ed567d6ca
Pedro Peres <pedro.peres@weightwatchers.com>
2017-02-15 20:36:09 -0800

    refactored parser and builder to work with tokens

--- lib/swift/boiler.rb
+++ lib/swift/boiler.rb
@@ -10,39 +10,13 @@ module Swift
 
       def boil(arguments)
         scanner = Swift::Boiler::Scanner.new
-        parser = Swift::Boiler::Builder.new
+        parser = Swift::Boiler::Parser.new
         builder = Swift::Boiler::Builder.new
         token_list = scanner.scan(arguments)
-
-        # action = arguments.shift
-        # parse_action(action, arguments)
+        template_info = parser.parse_tokens(token_list)
+        builder.build_template(template_info)
       end
-
-      def parse_action(action, arguments)
-        builder = Swift::Boiler::Builder.new
-        key = action.downcase
-        template = key + '.mustache'
-
-        shortcut_map = {
-            tableviewcell: 'table_view_cell',
-            m: 'model',
-            v: 'view',
-            c: 'controller',
-            s: 'singleton',
-            tvc: 'table_view_cell'
-        }
-
-        # default - look for templates that match this action
-        if builder.template_exists(template)
-          builder.boil_template(template, arguments)
-        elsif shortcut_map.has_key?(key.to_sym)
-            # shortcuts - look for specific actions
-            builder.boil_template(shortcut_map[key.to_sym] + '.mustache', arguments)
-        else
-            puts 'Could not find template for: ' + key
-        end
-
-      end
+      
     end
   end
 end
--- lib/swift/boiler/template.rb
+++ lib/swift/boiler/template.rb
@@ -3,9 +3,15 @@ module Swift
     class Template 
         attr_accessor :template_path, :class_name, :dev_name, :date, :properties, :options
 
-        def initialize (type, content)
+        def initialize
             @dev_name = "<#T##Developer Name ##Name#>"
             @date = Time.now.strftime("%d/%m/%Y")
+            @delegate = false
+            @options = Array.new
+            @properties = {}
+            @properties[:ui_label] = Array.new
+            @properties[:ui_view] = Array.new
+            @properties[:unknow_type] = Array.new
         end
 
     end 
